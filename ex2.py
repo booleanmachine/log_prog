d5c1019affffa91ed7591597ac0528ed17e803e4
Matheus Soeiro Pinheiro <art0072386@aluno.firjansenaisesi.com.br>
2026-09-10 19:20:10 -0300

    fazendo algumas verificações durante a execução do ex2

--- ex2.py
+++ ex2.py
@@ -5,11 +5,11 @@ num3 = float(input('Digite o terceiro numero: '))
 lista_numeros = [num1, num2, num3]
 
 maior_numero = 0
+if num1 == num2 and num2 == num3:
+    print('Os numeros são iguais')
 
-for i in lista_numeros:
-    if i > maior_numero:
-        maior_numero = i
-
-
-print(f'O maior numero é o numero: {maior_numero}')
-    
+if num1 != num2 and num3:
+    for i in lista_numeros:
+        if i > maior_numero:
+            maior_numero = i
+    print(f'O maior numero é o numero: {maior_numero} ')
